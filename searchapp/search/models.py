e2cf4eee1dafcf8815c6b29bcfbca49aabde7a23
debg48 <debgandhar4000@gmail.com>
2023-07-19 21:52:31 +0530

    db models added

--- searchapp/search/models.py
+++ searchapp/search/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Resturant(models.Model):
+    index = models.IntegerField()
+    id_no = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    items = models.TextField()
+    lat_long = models.TextField()
+    full_details = models.TextField()
